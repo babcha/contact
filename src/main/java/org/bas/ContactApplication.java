ac3be3daca4aac09ac3715adad588f53cb04f0b0
bassem <abchabassem@gmail.com>
2019-08-19 00:02:06 +0200

    backendPremiereVersion

--- src/main/java/org/bas/ContactApplication.java
+++ src/main/java/org/bas/ContactApplication.java
@@ -1,13 +1,28 @@
 package org.bas;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.bas.dao.ContactRepository;
+import org.bas.entities.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactApplication {
+public class ContactApplication implements CommandLineRunner {
 
+	@Autowired
+	ContactRepository contactRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ContactApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 
 }
---
+++ src/main/java/org/bas/entities/Contact.java
@@ -0,0 +1,90 @@
+package org.bas.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Contact implements Serializable{
+	
+	private static final long serialVersionUID = 1160283483957155790L;
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String nom;
+	private String prenom;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dateNaissance;
+	private String email;
+	private long tel;
+	private String photo;
+	
+	public Contact() {
+		super();
+	}
+	public Contact(String non, String prenom, Date dateNaissance, String email, long tel, String photo) {
+		super();
+		this.nom = non;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.email = email;
+		this.tel = tel;
+		this.photo = photo;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String non) {
+		this.nom = non;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getTel() {
+		return tel;
+	}
+	public void setTel(long tel) {
+		this.tel = tel;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	
+	
+	
+
+
+}
